bea483c39dfbc75afeace704eed7829c512736db
MusawwirMaqsood <musawwir58@gmail.com>
2021-04-10 17:58:18 +0500

    Fly component added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flame/util.dart';
 import 'package:flamegame/flamegame.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
   FlameGame game = FlameGame();
-  TapGestureRecognizer tapper = TapGestureRecognizer();
-  tapper.onTapDown = game.onTapDown;
   runApp(game.widget);
-  // ignore: deprecated_member_use
-  flameUtil.addGestureRecognizer(tapper);
 }
-
